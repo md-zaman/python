6a0c4eaaa2412020a73f6e49df07676143981d9d
Md Zaman <md.zaman4493@gmail.com>
2023-08-19 09:15:47 +0530

    Add fresh chapter

---
+++ mosh-hamedani/6_exceptions/1_exceptions.py
@@ -0,0 +1,13 @@
+# numbers = [1, 2]
+# print(number[3])
+"""
+Our program might encounter an error and terminate
+This might happen due to many reasons- there could be an 
+error in code, the user might end wront input or resources 
+might not be available e.g., we might want to open a file but 
+the file is not available and our program crashes
+
+It's a programmers resposibility to prevent a program from encountering these errors
+"""
+
+age = int(input("Age: "))
